c02963af5b789f38222487ae11035fd10bbc3ba0
MarkCesium <danikhrenkov29@gmail.com>
2025-03-03 21:52:32 +0300

    Fix db_helper

--- src/core/db_helper.py
+++ src/core/db_helper.py
@@ -1,34 +1,22 @@
 from typing import AsyncGenerator
 
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
-from sqlalchemy.orm import sessionmaker
 
 from src.core.config import settings
 
 
 class DataBaseHelper:
-    def __init__(self, async_url: str, sync_url: str, echo: bool) -> None:
+    def __init__(self, url: str, echo: bool) -> None:
         self.async_engine = create_async_engine(
-            url=async_url,
-            echo=echo,
-        )
-        self.sync_engine = create_engine(
-            url=sync_url,
+            url=url,
             echo=echo,
         )
         self.async_session_factory = async_sessionmaker(
             bind=self.async_engine,
-            autoflush=False,
-            autocommit=False,
-            expire_on_commit=False,
-        )
-        self.sync_session_factory = sessionmaker(
-            bind=self.sync_engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
@@ -42,4 +30,4 @@ class DataBaseHelper:
             yield session
 
 
-db_helper = DataBaseHelper(str(settings.database.url), str(settings.database.sync_url), settings.database.echo)
+db_helper = DataBaseHelper(str(settings.database.url), settings.database.echo)
