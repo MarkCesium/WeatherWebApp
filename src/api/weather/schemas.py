92e9ef551b0a7a2853475e460bd54a6d572289b3
MarkCesium <danikhrenkov29@gmail.com>
2025-03-03 23:54:18 +0300

    Fix schemas

--- src/api/weather/schemas.py
+++ src/api/weather/schemas.py
@@ -1,11 +1,25 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
-class WeatherResponse(BaseModel):
+class QueryRead(BaseModel):
+    id: int
     city: str
     temperature: float
     feels_like: float
     humidity: int
     wind_speed: float
     description: str
+    created_at: datetime
     
+    class Config:
+        from_attributes = True
+
+class QueryCreate(BaseModel):
+    city: str
+    temperature: float
+    feels_like: float
+    humidity: int
+    wind_speed: float
+    description: str
