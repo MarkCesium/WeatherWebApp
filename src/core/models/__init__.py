4ef7d0a7a37d4ea7a1d8f81862fa8eeaa151b494
MarkCesium <danikhrenkov29@gmail.com>
2025-03-03 23:18:09 +0300

    Create base and query models

---
+++ src/core/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import Base
+from .query import Query
+
+__all__ = (
+    "Base",
+    "Query",
+)
---
+++ src/core/models/query.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Query(Base):
+    __tablename__ = "queries"
+    
+    city: Mapped[str]
+    temperature: Mapped[float]
+    feels_like: Mapped[float]
+    humidity: Mapped[int]
+    wind_speed: Mapped[float]
+    description: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(DateTime(True), default=datetime.now(timezone.utc))
+    
